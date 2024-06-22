b339b7066277d3556381fba07f80f756b682f1b9
Tanner Davison <tanner.davison95@gmail.com>
2024-06-21 20:38:33 -0600

    added wow

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((1512, 982), vsync=1)
 
-class MyGameChar:
-    x = 100 #sets x position on screen
-    y = 100 #sets y position on screen
+class MainChar:
+    x = 100 
+    y = 100 
     width = 100
     height = 50
     color = (255, 0, 0)  
@@ -21,33 +21,33 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        MyGameChar.y -= MyGameChar.speed
+        MainChar.y -= MainChar.speed
     if keys[pygame.K_DOWN]:
-        MyGameChar.y += MyGameChar.speed
+        MainChar.y += MainChar.speed
     if keys[pygame.K_LEFT]:
-        MyGameChar.x -= MyGameChar.speed
+        MainChar.x -= MainChar.speed
     if keys[pygame.K_RIGHT]:
-        MyGameChar.x += MyGameChar.speed
+        MainChar.x += MainChar.speed
     if keys[pygame.K_w]:
-        MyGameChar.width
+        MainChar.width
 
 
-    if MyGameChar.x < 0:
-       MyGameChar.x = 0
-    if MyGameChar.x > screen.get_width() - MyGameChar.width:
-        MyGameChar.x = screen.get_width() - MyGameChar.width
-    if MyGameChar.y < 0:
-        MyGameChar.y = 0
-    if MyGameChar.y > screen.get_height() - MyGameChar.height-100:
+    if MainChar.x < 0:
+       MainChar.x = 0
+    if MainChar.x > screen.get_width() - MainChar.width:
+        MainChar.x = screen.get_width() - MainChar.width
+    if MainChar.y < 0:
+        MainChar.y = 0
+    if MainChar.y > screen.get_height() - MainChar.height-100:
         f = open("latest_moves.txt", "w")
         f.write("bottom border was hit!\n")
         f.close()
-        MyGameChar.y = screen.get_height() - MyGameChar.height-100
+        MainChar.y = screen.get_height() - MainChar.height-100
 
 
     screen.fill((255, 255, 255)) 
-    pygame.draw.rect(screen, MyGameChar.color, (MyGameChar.x, MyGameChar.y, MyGameChar.width, MyGameChar.height))
+    pygame.draw.rect(screen, MainChar.color, (MainChar.x, MainChar.y, MainChar.width, MainChar.height))
 
     pygame.display.flip()
-
+print('wow')
 pygame.quit()
